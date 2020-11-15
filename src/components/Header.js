4176534aec0d937d25e0df3d4dec7a6fcffbf592
Ryan Njoroge <keddelyronjoz@gmail.com>
2020-11-15 12:23:52 +0300

    Minor fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React, {Fragment} from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import Button from 'react-bootstrap/Button'
 
 import './Header.scss'
 
