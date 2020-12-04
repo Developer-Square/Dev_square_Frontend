53eb3207af0a4929a011668b9513806426e96002
Ryan Njoroge <keddelyronjoz@gmail.com>
2020-12-04 18:47:01 +0300

    Minor fix

--- src/Dashboard/pages/Clients/ClientsPage.js
+++ src/Dashboard/pages/Clients/ClientsPage.js
@@ -17,12 +17,14 @@ function ClientsPage() {
     const [modalShow, setModalShow] = useState(false);
 
     return (
+        <>
+        <AddButton onClick={() => setModalShow(true)}  />
         <Container className="clients">
-            <AddButton onClick={() => setModalShow(true)}  />
             <ClientModal show={modalShow} onHide={() => setModalShow(false)}/>
 			<Clients title="Active Clients" count={6} data={depositData.active} />
 			<Clients title="Closed Clients" count={3} data={depositData.closed} />
         </Container>
+        </>
     )
 }
 
