22b51560c98e5276d5411bedf8395a0750dbeafd
Ryan Njoroge <keddelyronjoz@gmail.com>
2020-12-06 21:48:42 +0300

    Done with auth/login, auth/logout

--- src/Routes.js
+++ src/Routes.js
@@ -10,6 +10,7 @@ import UsersPage from './Dashboard/pages/Users/UsersPage'
 import ClientsPage from './Dashboard/pages/Clients/ClientsPage'
 import PortfolioPage from './Dashboard/pages/Portfolio/PorfolioPage'
 import TasksPage from './Dashboard/pages/Tasks/TasksPage'
+import ResetPassword from './components/ResetPassword'
 import {ThemeContextProvider} from './context/themeContext'
 import RouteWithLayout from './RouteWithLayout'
 
@@ -19,6 +20,7 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={Homepage}/>
                 <Route path="/login" exact component={LoginAndSignUp}/>
+                <Route path="/reset/password" component={ResetPassword}/>
                 <ThemeContextProvider>
                     <RouteWithLayout path="/dashboard/home" exact component={DashboardHome} layout={Dashboard}/>
                     <RouteWithLayout path="/dashboard/users" exact component={UsersPage} layout={Dashboard}/>
--- src/components/LoginAndSignUp.js
+++ src/components/LoginAndSignUp.js
@@ -2,119 +2,160 @@ import React, {Fragment, useState} from 'react'
 import Form from 'react-bootstrap/Form'
 import cx from 'classnames'
 
+//Own Components
 import styles from './LoginAndSignUp.module.scss'
+import Api from '../services/network'
 
-function LoginAndSignUp() {
+function LoginAndSignUp({history}) {
+    const api = new Api()
 
     const [text, setText] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    //When the forgot state changes to true the form layout will shift
+    const [forgot, setForgot] = useState(false)
+    //When confirm is set to true the whole pages goes blank telling the user
+    //to check his/her email.
+    const [confirm, setConfirm] = useState(false)
 
     const handleClickSignUp = () => {
         setText(`${styles.sign_up_mode}`)
     } 
 
     const handleClickSignIn = () => {
-        setText('')
-    } 
+        setText(`${styles.sign_in_mode}`)
+        setEmail('')
+        setPassword('')
+
+    }
+
+    const clearFields = () => {
+        setEmail('')
+        setPassword('')
+    }
+
+    function handleLogin(e) {
+        e.preventDefault()
+        const credentials = {
+            email,
+            password
+        }
+
+        //Api Call
+        api.auth().login(credentials)
+        .then((res) => {
+            if (res.status === 200) {
+                localStorage.setItem('jwtToken', res.data.tokens.access.token)
+                localStorage.setItem('refreshToken', res.data.tokens.refresh.token)
+                 //Clear the inputs
+                clearFields()
+                history.push('/dashboard/home')
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    function handleForgotPassword(e) {
+        e.preventDefault()
+        const data = {
+            email
+        }
+        api.auth().forgotPassword(data)
+        setConfirm(true)
+    }
 
     return (
         <Fragment>
+            {confirm ? 
+            <div className={styles.confirm_page}>
+                <div>Please check your email for further details.</div>
+                <span>Cosmic Software Solutions</span>
+            </div>
+            :
             <div className={cx(styles.container, text)} id="container">
-                <div className={styles.forms_container}>
-                    <div className={styles.signin_signup}>
-                        <Form action="" className={styles.sign_in_form}>
+            <div className={styles.forms_container}>
+                <div className={styles.signin_signup}>
+                    <Form className={styles.sign_in_form}>
+                        {/* Changing the layout to bring out the forgot password form */}
+                        {forgot ? 
+                        <>
+                            <h2 className={styles.title}>Forgot Password</h2>
+                            <div className={styles.input_field}>
+                                <i className="fas fa-user"></i>
+                                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"/>
+                            </div>
+                            <div className={styles.buttons}>
+                                <input type="submit" value="Send" onClick={handleForgotPassword} className={styles.btn}/>
+                                <input type="button" value="Back" onClick={() => setForgot(false)} className={cx(styles.btn, styles.btn_forgot)}/>
+                            </div>
+                        </>
+                        :
+                        <>
                             <h2 className={styles.title}>Sign In</h2>
                             <div className={styles.input_field}>
                                 <i className="fas fa-user"></i>
-                                <input type="text" placeholder="Username"/>
+                                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"/>
                             </div>
                             <div className={styles.input_field}>
                                 <i className="fas fa-lock"></i>
-                                <input type="password" placeholder="Password"/>
+                                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"/>
                             </div>
+                            <p className={styles.social_text} onClick={() => setForgot(true)}>Forgot Password</p>
                             <div className={styles.buttons}>
-                            <input type="submit" value="Login" className={styles.btn}/>
-                            <a href="/" className={cx(styles.btn, styles.btn_back)}>Back to Homepage</a>
+                                <input type="submit" value="Login" onClick={handleLogin} className={styles.btn}/>
+                                <a href="/" className={cx(styles.btn, styles.btn_back)}>Back to Homepage</a>
                             </div>
+                        </>
+                        }
+                    </Form>
 
-                            {/* To be used later*/}
-                            {/* <p className={styles.social_text}>Or Sign In with social platforms</p>
-                            <div className={styles.social_media}>
-                                <a href="!#" className={styles.social_icon}>
-                                    <i className="fab fa-facebook-f"></i>
-                                </a>
-                                <a href="!#" className={styles.social_icon}>
-                                    <i className="fab fa-twitter"></i>
-                                </a>
-                                <a href="!#" className={styles.social_icon}>
-                                    <i className="fab fa-google"></i>
-                                </a>
-                                <a href="!#" className={styles.social_icon}>
-                                    <i className="fab fa-linkedin-in"></i>
-                                </a>
-                            </div> */}
-                        </Form>
-
-                        <Form action="" className={styles.sign_up_form}>
-                            <h2 className={styles.title}>Sign Up</h2>
-                            <div className={styles.input_field}>
-                                <i className="fas fa-user"></i>
-                                <input type="text" placeholder="Username"/>
-                            </div>
-                            <div className={styles.input_field}>
-                                <i className="fas fa-envelope"></i>
-                                <input type="text" placeholder="Email"/>
-                            </div>
-                            <div className={styles.input_field}>
-                                <i className="fas fa-lock"></i>
-                                <input type="password" placeholder="Password"/>
-                            </div>
-                            <div className={styles.buttons}>
-                            <input type="submit" value="Sign Up" className={styles.btn}/>
-                            <a href="/" className={cx(styles.btn, styles.btn_back)}>Back to Homepage</a>
-                            </div>
-
-                            {/* To be used later*/}
-                            {/* <p className="social-text">Or Sign Up with social platforms</p>
-                            <div className={styles.social_media}>
-                                <a href="!#" className={styles.social_icon}>
-                                    <i className="fab fa-facebook-f"></i>
-                                </a>
-                                <a href="!#" className={styles.social_icon}>
-                                    <i className="fab fa-twitter"></i>
-                                </a>
-                                <a href="!#" className={styles.social_icon}>
-                                    <i className="fab fa-google"></i>
-                                </a>
-                                <a href="!#" className={styles.social_icon}>
-                                    <i className="fab fa-linkedin-in"></i>
-                                </a>
-                            </div> */}
-                        </Form>
-                    </div>
-                </div>
-
-                <div className={styles.panels_container}>
-                    <div className={cx(styles.panel, styles.left_panel)}>
-                        <div className={styles.content}>
-                            <h3>New here ?</h3>
-                            <p>Why don't you sign up and join our incredible family. We would be glad to have you onboard</p>
-                            <button className={cx(styles.btn, styles.btn_transparent)} onClick={handleClickSignUp} id="sign-up-btn">Sign Up</button>
+                    <Form action="" className={styles.sign_up_form}>
+                        <h2 className={styles.title}>Sign Up</h2>
+                        <div className={styles.input_field}>
+                            <i className="fas fa-user"></i>
+                            <input type="text" placeholder="Username"/>
                         </div>
-
-                        <img src="login-svg/undraw_project_team_lc5a.svg" className={styles.image} alt="signin"/>
-                    </div>
-
-                    <div className={cx(styles.panel, styles.right_panel)}>
-                        <div className={styles.content}>
-                            <h3>One of Us ?</h3>
-                            <p>Welcome back, glad to have you a board.</p>
-                            <button className={cx(styles.btn, styles.btn_transparent)} onClick={handleClickSignIn} id="sign-in-btn">Sign In</button>
+                        <div className={styles.input_field}>
+                            <i className="fas fa-envelope"></i>
+                            <input type="text" placeholder="Email"/>
                         </div>
-
-                        <img src="login-svg/undraw_startup_life_2du2.svg" className={styles.image} alt="signup"/>
-                    </div>
+                        <div className={styles.input_field}>
+                            <i className="fas fa-lock"></i>
+                            <input type="password" placeholder="Password"/>
+                        </div>
+                        <div className={styles.buttons}>
+                        <input type="submit" value="Sign Up" className={styles.btn}/>
+                        <a href="/" className={cx(styles.btn, styles.btn_back)}>Back to Homepage</a>
+                        </div>
+                    </Form>
                 </div>
             </div>
+
+            <div className={styles.panels_container}>
+                <div className={cx(styles.panel, styles.left_panel)}>
+                    <div className={styles.content}>
+                        <h3>New here ?</h3>
+                        <p>Why don't you sign up and join our incredible family. We would be glad to have you onboard</p>
+                        <button className={cx(styles.btn, styles.btn_transparent)} onClick={handleClickSignUp} id="sign-up-btn">Sign Up</button>
+                    </div>
+
+                    <img src="login-svg/undraw_project_team_lc5a.svg" className={styles.image} alt="signin"/>
+                </div>
+
+                <div className={cx(styles.panel, styles.right_panel)}>
+                    <div className={styles.content}>
+                        <h3>One of Us ?</h3>
+                        <p>Welcome back, glad to have you a board.</p>
+                        <button className={cx(styles.btn, styles.btn_transparent)} onClick={handleClickSignIn} id="sign-in-btn">Sign In</button>
+                    </div>
+
+                    <img src="login-svg/undraw_startup_life_2du2.svg" className={styles.image} alt="signup"/>
+                </div>
+            </div>
+        </div>
+        }
         </Fragment>
     )
 }
