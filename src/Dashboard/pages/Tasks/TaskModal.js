8acf3e305f719bf23d250d2c411cbc8dc201a437
Ryan Njoroge <keddelyronjoz@gmail.com>
2020-12-08 13:59:28 +0300

    Started on task and fixed the errors

--- src/Dashboard/pages/Tasks/TaskModal.js
+++ src/Dashboard/pages/Tasks/TaskModal.js
@@ -3,11 +3,17 @@ import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
+//Own Components
+import Api from '../../../services/network'
+import IsEmpty from '../../../helpers/IsEmpty'
+import notify from '../../../helpers/Notify'
+
 export default function TaskModal(props) {
-    const [ , setName] = useState('')
+    const [description, setDescription] = useState('')
     const [status, setStatus] = useState('')
     const [dueDate, setDueDate] = useState('')
     const [stack, setStack] = useState('')
+    const [difficulty, setDifficulty] = useState('')
     const [validated, setValidated] = useState(false)
 
     function handleSubmit(e, props) {
@@ -18,10 +24,11 @@ export default function TaskModal(props) {
         } else {
             setValidated(true)
             let data = {
-                name,
+                description,
                 status,
                 dueDate,
-                password
+                difficulty,
+                stack
             }
             
             //Checking if the data is empty with the helper function
@@ -32,12 +39,12 @@ export default function TaskModal(props) {
                 api.auth().registerUser(data)
                 .then(res => {
                     if (res.status === 201) {
-                        notify('success', 'User successfully created')
+                        notify('success', 'Task successfully created')
                     }
                 })
                 .catch(err => {
                     const {message} = err.response.data
-                    const customMessage = `User not created! \n ${message}`
+                    const customMessage = `Task not created! \n ${message}`
                     notify('error', customMessage)
                 })
             }
@@ -61,14 +68,14 @@ export default function TaskModal(props) {
                 <Form noValidate validated={validated} {...props}>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Task Name</Form.Label>
-                        <Form.Control required type="text" placeholder="Task Name..." />
+                        <Form.Control required type="text" onChange={(e) => setDescription(e.target.value)} placeholder="Task Name..." />
                         <Form.Control.Feedback type="invalid">
                             Please fill in the task name.
                         </Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Status</Form.Label>
-                        <Form.Control required as="select">
+                        <Form.Control onChange={(e) => setStatus(e.target.value)} required as="select">
                             <option>In Progress</option>
                             <option>On Hold</option>
                             <option>Not Started</option>
@@ -77,17 +84,25 @@ export default function TaskModal(props) {
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Due Date</Form.Label>
-                        <Form.Control required type="date" placeholder="date" />
+                        <Form.Control onChange={(e) => setDueDate(e.target.value)} required type="date" placeholder="date" />
                         <Form.Control.Feedback type="invalid">
                             Please fill in the due date.
                         </Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Stack</Form.Label>
-                        <Form.Control required type="text" placeholder="Stack..." />
+                        <Form.Control required onChange={(e) => setStack(e.target.value)} type="text" placeholder="Stack..." />
                         <Form.Control.Feedback type="invalid">
                             Please fill in the stack
                         </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicDifficulty">
+                        <Form.Label>Difficulty</Form.Label>
+                        <Form.Control onChange={(e) => setDifficulty(e.target.value)} required as="select">
+                            <option>Easy</option>
+                            <option>Medium</option>
+                            <option>Hard</option>
+                        </Form.Control>
                     </Form.Group>
                     <Button variant="primary" type="submit" onClick={(e) => handleSubmit(e, props)}>
                         Submit
