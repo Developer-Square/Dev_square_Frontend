efbd5f6492337250a32b86dce23f4cedaaf82c9f
ryann254 <keddelyronjoz@gmail.com>
2020-12-08 12:57:44 +0300

    Started on tasks

--- src/Dashboard/pages/Tasks/TaskModal.js
+++ src/Dashboard/pages/Tasks/TaskModal.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
 export default function TaskModal(props) {
+    const [name, setName] = useState('')
+    const [status, setStatus] = useState('')
+    const [dueDate, setDueDate] = useState('')
+    const [stack, setStack] = useState('')
+    const [validated, setValidated] = useState(false)
+
+    function handleSubmit(e, props) {
+        e.preventDefault()
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.stopPropagation()
+        } else {
+            setValidated(true)
+            const name = fname.concat(' ', lname)
+            let data = {
+                email,
+                name,
+                password
+            }
+            
+            //Checking if the data is empty with the helper function
+            if (IsEmpty(data) === true) {
+                //Hide the modal if the data is Not empty
+                props.onHide()
+                const api = new Api()
+                api.auth().registerUser(data)
+                .then(res => {
+                    if (res.status === 201) {
+                        notify('success', 'User successfully created')
+                    }
+                })
+                .catch(err => {
+                    const {message} = err.response.data
+                    const customMessage = `User not created! \n ${message}`
+                    notify('error', customMessage)
+                })
+            }
+        }
+    }
+
     return (
         <Modal
         {...props}
@@ -18,14 +58,17 @@ export default function TaskModal(props) {
             </Modal.Header>
             <Modal.Body>
                 <h5 className="text-center">Fill the form below</h5>
-                <Form>
+                <Form noValidate validated={validated} {...props}>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Task Name</Form.Label>
-                        <Form.Control type="text" placeholder="Task Name..." />
+                        <Form.Control required type="text" placeholder="Task Name..." />
+                        <Form.Control.Feedback type="invalid">
+                            Please fill in the task name.
+                        </Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Status</Form.Label>
-                        <Form.Control as="select">
+                        <Form.Control required as="select">
                             <option>In Progress</option>
                             <option>On Hold</option>
                             <option>Not Started</option>
@@ -34,13 +77,19 @@ export default function TaskModal(props) {
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Due Date</Form.Label>
-                        <Form.Control type="date" placeholder="Password" />
+                        <Form.Control required type="date" placeholder="date" />
+                        <Form.Control.Feedback type="invalid">
+                            Please fill in the due date.
+                        </Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Stack</Form.Label>
-                        <Form.Control type="text" placeholder="Stack..." />
+                        <Form.Control required type="text" placeholder="Stack..." />
+                        <Form.Control.Feedback type="invalid">
+                            Please fill in the stack
+                        </Form.Control.Feedback>
                     </Form.Group>
-                    <Button variant="primary" type="submit">
+                    <Button variant="primary" type="submit" onClick={(e) => handleSubmit(e, props)}>
                         Submit
                     </Button>
                 </Form>
