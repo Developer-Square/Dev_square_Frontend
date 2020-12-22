6615245f85f88490bd5eb120a76c2d9eb27f6257
Ryan Njoroge <keddelyronjoz@gmail.com>
2020-12-22 16:54:16 +0300

    Changed the carousel images to webp

--- src/components/Reusable Components/CarouselComponent.js
+++ src/components/Reusable Components/CarouselComponent.js
@@ -14,7 +14,7 @@ export default function CarouselComponent({packageType}) {
             <img
             className="d-block w-100"
             // Choosing which image to display depending on the props
-            src={`images/pricing-images/${packageType === 'basic' ? 'Business-basic-1' : packageType === 'classic' ? 'Bakery-classic-1': 'Interior Design2'}.png`}
+            src={`images/pricing-images/${packageType === 'basic' ? 'Business-basic-1' : packageType === 'classic' ? 'Bakery-classic-1': 'Interior Design2'}.webp`}
             alt="First slide"
             />
         </Carousel.Item>
@@ -22,7 +22,7 @@ export default function CarouselComponent({packageType}) {
             <img
             className="d-block w-100"
             // Choosing which image to display depending on the props
-            src={`images/pricing-images/${packageType === 'basic' ? 'Business-basic-2' : packageType === 'classic' ? 'Bakery-classic-2': 'Interior Design1'}.png`}
+            src={`images/pricing-images/${packageType === 'basic' ? 'Business-basic-2' : packageType === 'classic' ? 'Bakery-classic-2': 'Interior Design1'}.webp`}
             alt="First slide"
             />
         </Carousel.Item>
@@ -30,7 +30,7 @@ export default function CarouselComponent({packageType}) {
             <img
             className="d-block w-100"
             // Choosing which image to display depending on the props
-            src={`images/pricing-images/${packageType === 'basic' ? 'Business-basic-3' : packageType === 'classic' ? 'Bakery-classic-3': 'Interior Design3'}.png`}
+            src={`images/pricing-images/${packageType === 'basic' ? 'Business-basic-3' : packageType === 'classic' ? 'Bakery-classic-3': 'Interior Design3'}.webp`}
             alt="First slide"
             />
         </Carousel.Item>
@@ -38,7 +38,7 @@ export default function CarouselComponent({packageType}) {
             <img
             className="d-block w-100"
             // Choosing which image to display depending on the props
-            src={`images/pricing-images/${packageType === 'basic' ? 'Business-basic-4' : packageType === 'classic' ? 'Bakery-classic-4': 'Wedding-planner-unique1'}.png`}
+            src={`images/pricing-images/${packageType === 'basic' ? 'Business-basic-4' : packageType === 'classic' ? 'Bakery-classic-4': 'Wedding-planner-unique1'}.webp`}
             alt="First slide"
             />
         </Carousel.Item>
@@ -46,7 +46,7 @@ export default function CarouselComponent({packageType}) {
             <img
             className="d-block w-100"
             // Choosing which image to display depending on the props
-            src={`images/pricing-images/${packageType === 'basic' ? 'Law-Basic-1' : packageType === 'classic' ? 'Bakery-classic2-1': 'Wedding-planner-unique2'}.png`}
+            src={`images/pricing-images/${packageType === 'basic' ? 'Law-Basic-1' : packageType === 'classic' ? 'Bakery-classic2-1': 'Wedding-planner-unique2'}.webp`}
             alt="First slide"
             />
         </Carousel.Item>
@@ -54,7 +54,7 @@ export default function CarouselComponent({packageType}) {
             <img
             className="d-block w-100"
             // Choosing which image to display depending on the props
-            src={`images/pricing-images/${packageType === 'basic' ? 'Law-Basic-2' : packageType === 'classic' ? 'Bakery-classic2-2': 'Wedding-planner-unique3'}.png`}
+            src={`images/pricing-images/${packageType === 'basic' ? 'Law-Basic-2' : packageType === 'classic' ? 'Bakery-classic2-2': 'Wedding-planner-unique3'}.webp`}
             alt="First slide"
             />
         </Carousel.Item>
@@ -62,7 +62,7 @@ export default function CarouselComponent({packageType}) {
             <img
             className="d-block w-100"
             // Choosing which image to display depending on the props
-            src={`images/pricing-images/${packageType === 'basic' ? 'Law-Basic-3' : packageType === 'classic' ? 'Bakery-classic2-3': 'Wedding-planner-unique4'}.png`}
+            src={`images/pricing-images/${packageType === 'basic' ? 'Law-Basic-3' : packageType === 'classic' ? 'Bakery-classic2-3': 'Wedding-planner-unique4'}.webp`}
             alt="First slide"
             />
         </Carousel.Item>
