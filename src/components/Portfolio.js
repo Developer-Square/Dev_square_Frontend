ce49547ce681e086baf2f283e2f899081dc4d4ea
Ryan Njoroge <45309245+ryann254@users.noreply.github.com>
2020-12-30 09:48:08 +0300

    Portfolio refactor (#19)
    * Done with adding images to portfolio section
    
    * Done with portfolio refactor
    
    * Converter services pics to webp
    
    * Restored portfolio-images

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,29 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState, useEffect} from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
+//Own Components
 import './Portfolio.scss'
+import PortfolioList from './PortfolioList'
 
 function Portfolio() {
+    const [option, setOption] = useState('')
+
+    useEffect(() => {
+        setOption('all')
+    }, [])
+
+    function handleChange (e) {
+        const val = e.target.innerHTML.toLowerCase()
+        const options = document.querySelectorAll('.option')
+        Array.from(options).map(opt => {
+            opt.classList.remove('active')
+            return null;
+        })
+        e.target.classList.add('active')
+        setOption(val)
+    }
     return (
         <Fragment>
             <Container id="portfolio-section">
@@ -14,43 +32,15 @@ function Portfolio() {
                         <div className="portfolio">
                             <div className="heading text-center">portfolio</div>
                             <div className="d-flex portfolio-header mx-auto justify-content-around">
-                                <p>ALL</p>
-                                <p>BUSINESS TYPE</p>
-                                <p>PHOTOGRAPHY</p>
-                                <p>WEDDING</p>
-                                <p className="fashion-lg-screens">FASHION</p>
+                                <p onClick={handleChange} className="option active">ALL</p>
+                                <p onClick={handleChange} className="option">BUSINESS TYPE</p>
+                                <p onClick={handleChange} className="option">PHOTOGRAPHY</p>
+                                <p onClick={handleChange} className="option">WEDDING</p>
+                                <p onClick={handleChange} className="fashion-lg-screens option">FASHION</p>
                             </div>
                             <div className="d-flex justify-content-center fashion-sm-screens"><p>FASHION</p></div>
                             <div className="portfolio-images mx-xl-auto">
-                                <Row className="mx-auto">
-                                    <Col xs={12} sm={7} md={7} lg={3} className="p-sm-0 image-container-large">
-                                        <img src="images/portfolio-1.webp" alt="portfolio" className="portfolio-1 img-fluid"/>
-                                    </Col>
-                                    <Col sm={5} md={5}  lg={3} className="larger-screens bank-image p-sm-0">
-                                        <img src="images/portfolio-2.webp" alt="portfolio" className="portfolio-2 img-fluid"/>
-                                        <img src="images/image-cover.webp" alt="portfolio" className="portfolio-2 image-cover img-fluid"/>
-                                        <img src="images/plus-icon.webp" alt="portfolio" className="portfolio-2 plus-icon img-fluid"/>
-                                        <p className="image-text">design name</p>
-                                    </Col>
-                                    <Col xs={12} sm={7} lg={3} className="p-sm-0 image-container-large">
-                                        <img src="images/portfolio-3.webp" alt="portfolio" className="portfolio-1 img-fluid"/>
-                                    </Col>
-                                    <Col sm={5} lg={3} className="larger-screens p-sm-0">
-                                        <img src="images/portfolio-4.webp" alt="portfolio" className="portfolio-2 img-fluid"/>
-                                    </Col>
-                                    <Col sm={5} lg={3} className="larger-screens p-sm-0">
-                                        <img src="images/portfolio-5.webp" alt="portfolio" className="portfolio-2 img-fluid"/>
-                                    </Col>
-                                    <Col xs={12} sm={7} lg={3} className="p-sm-0 image-container-large">
-                                        <img src="images/portfolio-6.webp" alt="portfolio" className="portfolio-1 img-fluid"/>
-                                    </Col>
-                                    <Col sm={5} lg={3} className="larger-screens p-sm-0">
-                                        <img src="images/portfolio-7.webp" alt="portfolio" className="portfolio-2 img-fluid"/>
-                                    </Col>
-                                    <Col xs={12} sm={7} lg={3} className="p-sm-0 image-container-large">
-                                        <img src="images/portfolio-8.webp" alt="portfolio" className="portfolio-1 img-fluid"/>
-                                    </Col>
-                                </Row>
+                                <PortfolioList portfolioType={option} />
                             </div>
                         </div>
                         <div className="scroll-bar portfolio-bar mt-2 mx-auto">
