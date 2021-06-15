69f957b705c332ba9343584dd51248a3b494a0e7
Ryan Njoroge <keddelyronjoz@gmail.com>
2021-06-15 21:18:26 +0300

    feat: Added test for element text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,18 +19,18 @@ function Header() {
             <Navbar className="mx-auto" expand="md" variant="dark">
                 <Navbar.Brand href="#home" onClick={() => scrollFunction('landing-section', 0)}>
                     <img src="images/Logo-2.webp" className="img-fluid" alt="Logo" data-test="logoImg"/>
-                    <span className="logo-text pl-2" data-test="logoText">{headername}</span></Navbar.Brand>
+                    <span className="logo-text pl-2" data-testid="logoText">{headername}</span></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav data-test="headerNav">
-                        <Nav.Link href="#home" className="mx-auto" onClick={() => scrollFunction('landing-section', 0)}><div className="indicator ml-md-2"></div>Home<div className="indicator-2 ml-md-2"></div></Nav.Link>
-                        <Nav.Link href="#about" className="mx-auto" onClick={() => scrollFunction('aboutus-section', 75)}><div className="indicator ml-md-4"></div>About us<div className="indicator-2 ml-md-4"></div></Nav.Link>
-                        <Nav.Link href="#portfolio" className="mx-auto" onClick={() => scrollFunction('portfolio-section', 75)}><div className="indicator ml-md-4"></div>Portfolio<div className="indicator-2 ml-md-4"></div></Nav.Link>
-                        <Nav.Link href="#services" className="mx-auto" onClick={() => scrollFunction('product-section', 75)}><div className="indicator ml-md-4"></div>Services<div className="indicator-2 ml-md-4"></div></Nav.Link>
-                        <Nav.Link href="#clients" className="mx-auto" onClick={() => scrollFunction('client-section', 75)}><div className="indicator ml-md-3"></div>Clients<div className="indicator-2 ml-md-3"></div></Nav.Link>
-                        <Nav.Link href="#Team" className="mx-auto" onClick={() => scrollFunction('team-section', 75)}><div className="indicator ml-md-2"></div>Team<div className="indicator-2 ml-md-2"></div></Nav.Link>
-                        <Nav.Link href="#blog" className="mx-auto" onClick={() => scrollFunction('blog-section', 75)}><div className="indicator ml-md-2"></div>Blog<div className="indicator-2 ml-md-2"></div></Nav.Link>
-                        <Nav.Link href="#contact" className="mx-auto contact" onClick={() => scrollFunction('contact-section', 75)}><div className="indicator ml-md-3"></div>Contact<div className="indicator-2 ml-md-3"></div></Nav.Link>
+                        <Nav.Link data-testid="home" href="#home" className="mx-auto" onClick={() => scrollFunction('landing-section', 0)}><div className="indicator ml-md-2"></div>Home<div className="indicator-2 ml-md-2"></div></Nav.Link>
+                        <Nav.Link data-testid="about us" href="#about" className="mx-auto" onClick={() => scrollFunction('aboutus-section', 75)}><div className="indicator ml-md-4"></div>About us<div className="indicator-2 ml-md-4"></div></Nav.Link>
+                        <Nav.Link data-testid="portfolio" href="#portfolio" className="mx-auto" onClick={() => scrollFunction('portfolio-section', 75)}><div className="indicator ml-md-4"></div>Portfolio<div className="indicator-2 ml-md-4"></div></Nav.Link>
+                        <Nav.Link data-testid="services" href="#services" className="mx-auto" onClick={() => scrollFunction('product-section', 75)}><div className="indicator ml-md-4"></div>Services<div className="indicator-2 ml-md-4"></div></Nav.Link>
+                        <Nav.Link data-testid="clients" href="#clients" className="mx-auto" onClick={() => scrollFunction('client-section', 75)}><div className="indicator ml-md-3"></div>Clients<div className="indicator-2 ml-md-3"></div></Nav.Link>
+                        <Nav.Link data-testid="team" href="#Team" className="mx-auto" onClick={() => scrollFunction('team-section', 75)}><div className="indicator ml-md-2"></div>Team<div className="indicator-2 ml-md-2"></div></Nav.Link>
+                        <Nav.Link data-testid="blog" href="#blog" className="mx-auto" onClick={() => scrollFunction('blog-section', 75)}><div className="indicator ml-md-2"></div>Blog<div className="indicator-2 ml-md-2"></div></Nav.Link>
+                        <Nav.Link data-testid="contact" href="#contact" className="mx-auto contact" onClick={() => scrollFunction('contact-section', 75)}><div className="indicator ml-md-3"></div>Contact<div className="indicator-2 ml-md-3"></div></Nav.Link>
                         <Nav.Link href="/login" className="mx-auto pl-2 pr-2 mt-2 mt-md-0 login">Login</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {shallow} from 'enzyme'
 import { render } from '@testing-library/react';
+import { screen } from '@testing-library/dom';
 import Header from './Header'
 import {findByTestAttr} from '../../testing-utils/index'
 
@@ -9,11 +10,18 @@ const setUp = (props={}) => {
     return component
 }
 
-const testingText = (text) => {
-    const {getByText} = render(<Header />)
-    const logoText = getByText(`${text}`)
-    return logoText
+// Testing an element's text.
+const testingMultipleElements = (id, text, type) => {
+    render(<Header />)
+    if (type === 'multiple') {
+        expect(screen.getAllByTestId(`${id}`)[0]).toHaveTextContent(`${text}`);
+    } else {
+        expect(screen.getByTestId(`${id}`)).toHaveTextContent(`${text}`);
+    }
 }
+
+let navText = ['Home', 'About us', 'Portfolio', 'Services', 'Clients', 'Team', 'Blog', 'Contact']
+
 
 describe('Header Component', () => {
     let component
@@ -27,12 +35,17 @@ describe('Header Component', () => {
     })
 
     it('Should render logo text', () => {
-        const text = testingText('tecHive')
-        expect(text).toBeInTheDocument()
+        testingMultipleElements('logoText', 'tecHive')
     })
 
     it('Should render the nav menu', () => {
         const wrapper = findByTestAttr(component, 'headerNav')
         expect(wrapper.length).toBe(1)
+    })
+
+    it('Should render home nav text', () => {
+        navText.map(item => {
+            const text = testingMultipleElements(item.toLowerCase(), item, 'multiple')
+        })
     })
 })
