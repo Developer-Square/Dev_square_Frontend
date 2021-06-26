b004cd6e841fd394d6a902e0356b6e08b93eb060
clinton wambugu <clintonwambugu@gmail.com>
2021-06-26 23:53:09 +0300

    Refactor: display list in serviceitem component

--- src/components/ServiceItem.js
+++ src/components/ServiceItem.js
@@ -3,24 +3,25 @@ import Col from 'react-bootstrap/Col';
 
 import './ServicesAndBlog.scss'
 
-function ServiceItem(props) {
-  
-  {/* props for image, title, description classname and array of offerings*/ }
+export default function ServiceItem({ img1, img2, title, description, descClass, imgClass, optionalList }) {
+
   return (
     <Col xs={12} sm={6} className="service-container">
       <div className="service-title d-flex justify-content-center">
-        <img src={props.image1} alt="services" className="img-fluid" />
-        <img src={props.image2} alt="services" className="service-icon img-fluid" />
-        <p className="title my-auto">{props.title}</p>
+        <img src={`images/${img1}.webp`} alt="services" className="img-fluid" />
+        <img src={img2} alt="services" className={`${imgClass} img-fluid`} />
+        <p className="title my-auto">{title}</p>
       </div>
-      <div className={`${props.classname} p-3`}>
-        <p>{props.description}</p>
-        <ul>
-          { props.offering?  props.offering.map( item => (<li>{item}</li>)) : null}
-        </ul>
+      <div className={`${descClass} p-3`}>
+        <p>{description}</p>
+
+        {optionalList.length !== 0 ? optionalList.map(item => (
+          <ul>
+            <li>{item}</li>
+          </ul>
+        )) : null}
       </div>
     </Col>
   );
 }
 
-export default ServiceItem;
--- src/components/ServicesAndBlog.js
+++ src/components/ServicesAndBlog.js
@@ -7,6 +7,34 @@ import ServiceItem from './ServiceItem';
 import './ServicesAndBlog.scss'
 
 function ServicesAndBlog() {
+    const images = [["services-shape-1", "images/assets/icons/nodejs.webp"], ["services-shape-2", "images/services-icon-2.webp"], ["services-shape-2","images/services-icon-3.webp"], ["services-shape-2","images/assets/icons/mobile-app.webp"], ["services-shape-2","images/assets/icons/reactjs-48.webp"], ["services-shape-1","images/assets/icons/ui-ux-design-2.webp"], ["services-shape-2","images/assets/icons/php.webp"], ["services-shape-2","images/services-icon-6.webp"], ["services-shape-1","images/assets/icons/ruby.webp"], ["services-shape-1","images/assets/icons/debug.webp"], ["services-shape-2","images/assets/icons/django.webp"], ["services-shape-2","images/assets/icons/data-analyst.webp"]];
+    
+    const titles = ["HTML/CSS", "WORDPRESS", "Mobile APPLICATIONS", "Reactjs APPLICATIONS", "UI/UX DESIGN", "RESPONSIVE LAYOUT", "Ruby on Rails", "Debugging Code", "Django Development", "Data Analyst"];
+    
+    const descriptions = [
+        "We create full Nodejs applications with features you as the user can specify. You could also choose with Nodejs backend with a React frontend", 
+        "We also create websites using bare HTML, Css, Js, Bootstrap and Jquery for the animations.These websites are mostly created from scratch.We can also work with other frameworks like AntDesign and Evergreen if requested to.",
+        "We can also create a wordpress website for you with specified plugins. Most requested wordpress websites are E-commerce websites, we can create those too.",
+        "We go the full cycle by not only providing you with a website but an app too that your users can use on their phones to access your business/services.We will build any android app for you provided the logic is explained and understood. If the app doesn't meet all the requirement mentioned we will revise at zero additional cost.",
+        "We create fully functional frontend applications using reactjs, you can also choose to add a backend to your react app using django or Nodejs.",
+        "If you choose to work with us, we provide you with a free design that can be customized to fit you goals.",
+        "We also deal in full stack PHP applications e.g If you'd like your E-commerce website built in PHP, we got you covered.We will build you a php ecommerce website for your business that can be easily customed as per your needs at an affordable price.",
+        "All of our websites come with a responsive layout, this means your website can be viewed on both large screen devices like laptops and computers and also mobile phones.",
+        "We've an experienced Ruby on rails developer that'll take care of all of your Ruby on rails needs.Ruby on Rails framework uses MVC approach to build web applications, It builds web applications quickly and the biggest strength is that it enables the developer to write big applications in a short period of time.What we're offering:",
+        "We have numerous engineers on our team all with quality experience in their fields and we can assist you in debugging your code.We can debung the following technologies:",
+        "For more dynamic work we will create your backend in django.",
+        " If you happen to have any data analyst enquiries, our data analyst will be ready to answer any of your questions."
+    ];
+    
+    const descClassNames  = ["inner-content-dev", "inner-content", "inner-content", "inner-content","inner-content", "inner-content", "inner-content", "inner-content", "inner-content-dev", "inner-content-dev", "inner-content", "inner-content"];
+    
+    const imgClassNames = ["service-icon", "service-icon-html", "service-icon-word", "service-icon-app", "service-icon-app", "service-icon-ui", "service-icon-responsive", "service-icon-responsive", "service-icon-ui", "service-icon-responsive", "service-icon-responsive", "service-icon-ui", "service-icon-responsive"]; //13 check afterwards
+    
+    const offerings1 = ["ROR Custom application development", "Ruby MVP Development", "Ruby on Rails layout design", "ROR installation", "Ruby on Rails CMS & Responsive web portal development", "Maintenance and deployment", "RoR eCommerce design", "Web API Development"];
+    
+    const offerings2 = ["HTML, Css", "Javascript", "Reactjs", "Ruby on Rails", "Nodejs", "Django", "PHP", "Android"];
+    
+
     return (
         <Fragment>
             <Container id="services-section">
@@ -16,117 +44,17 @@ function ServicesAndBlog() {
                         <p className="text-center">Our company provides the services stated below in more detail. It's our company's motto to deliver quality and maintainable websites and apps that please our users.
                         </p>
                         <Row className="limit mx-auto">
-                        
-                            <ServiceItem
-                                image1="images/services-shape-1.webp"
-                                image2="images/assets/icons/nodejs.webp"
-                                title="Nodejs Applications"
-                                description="We create full Nodejs applications with features you as the user can specify. You could also choose with Nodejs backend with a React frontend"
-                                classname="inner-content-dev"
-                            />
-
-                            <ServiceItem
-                                image1="images/services-shape-2.webp"
-                                image2="images/services-icon-2.webp"
-                                title="HTML/CSS"
-                                description="We also create websites using bare HTML, Css, Js, Bootstrap and Jquery for the animations.
-                                These websites are mostly created from scratch. We can also work with other frameworks like AntDesign and Evergreen if requested to."
-                                classname="inner-content"
-                            />
-
-                            <ServiceItem
-                                image1="images/services-shape-2.webp"
-                                image2="images/services-icon-3.webp"
-                                title="WORDPRESS"
-                                description="We can also create a wordpress website for you with specified plugins. Most requested wordpress websites are E-commerce websites, we can create those too."
-                                classname="inner-content"
-                            />
-
-                            <ServiceItem
-                                image1="images/services-shape-2.webp"
-                                image2="images/assets/icons/mobile-app.webp"
-                                title="Mobile APPLICATIONS"
-                                description="We go the full cycle by not only providing you with a website but an app too that your users can use on their phones to access your business/services.We will build any android app for you provided the logic is explained and understood. If the app doesn't meet all the requirement mentioned we will revise at zero additional cost."
-                                classname="inner-content"
-                            />
-
-                            <ServiceItem
-                                image1="images/services-shape-2.webp"
-                                image2="images/assets/icons/reactjs-48.webp"
-                                title="Reactjs APPLICATIONS"
-                                description="We create fully functional frontend applications using reactjs, you can also choose to add a backend to your react app using django or Nodejs."
-                                classname="inner-content"
-                            />
-
-                            <ServiceItem
-                                image1="images/services-shape-1.webp"
-                                image2="images/assets/icons/ui-ux-design-2.webp"
-                                title="UI/UX DESIGN"
-                                description="If you choose to work with us, we provide you with a free design that can be customized to fit you goals."
-                                classname="inner-content-dev"
-                            />
-
-                            <ServiceItem
-                                image1="images/services-shape-2.webp"
-                                image2="images/assets/icons/php.webp"
-                                title="PHP"
-                                description="We also deal in full stack PHP applications e.g If you'd like your E-commerce website built in PHP, we got you covered.We will build you a php ecommerce website for your business that can be easily customed as per your needs at an affordable price."
-                                classname="inner-content"
-                            />
-
-                            <ServiceItem
-                                image1="images/services-shape-2.webp"
-                                image2="images/services-icon-6.webp"
-                                title="RESPONSIVE LAYOUT"
-                                description="All of our websites come with a responsive layout, this means your website can be viewed on both large screen devices like laptops and computers and also mobile phones."
-                                classname="inner-content"
-                            />
-
-                            <ServiceItem
-                                image1="images/services-shape-1.webp"
-                                image2="images/services-icon-6.webp"
-                                title="RESPONSIVE LAYOUT"
-                                description="All of our websites come with a responsive layout, this means your website can be viewed on both large screen devices like laptops and computers and also mobile phones."
-                                classname="inner-content"
-                            />
-
-                            <ServiceItem
-                                image1="images/services-shape-1.webp"
-                                image2="images/assets/icons/ruby.webp"
-                                title="Ruby on Rails"
-                                description="We've an experienced Ruby on rails developer that'll take care of all of your Ruby on rails needs.Ruby on Rails framework uses MVC approach to build web applications, It builds web applications quickly and the biggest strength is that it enables the developer to write big applications in a short period of time.
-                                What we're offering:"
-                                offering={["ROR Custom application development", "Ruby MVP Development", "Ruby on Rails layout design", "ROR installation", "Ruby on Rails CMS & Responsive web portal development", "Maintenance and deployment", "RoR eCommerce design", "Web API Development"]}
-                                classname="inner-content-dev"
-                            />
-
-                            <ServiceItem
-                                image1="images/services-shape-1.webp"
-                                image2="images/assets/icons/debug.webp"
-                                title="Debugging Code"
-                                description="We have numerous engineers on our team all with quality experience in their fields and
-                                we can assist you in debugging your code.
-                                We can debung the following technologies:"
-                                offering={["HTML, Css", "Javascript", "Reactjs", "Ruby on Rails", "Nodejs", "Django", "PHP", "Android"]}
-                                classname="inner-content-dev"
-                            />
-
-                            <ServiceItem
-                                image1="images/services-shape-2.webp"
-                                image2="images/assets/icons/django.webp"
-                                title="Django Development"
-                                description="For more dynamic work we will create your backend in django."
-                                classname="inner-content"
-                            />
-
-                            <ServiceItem
-                                image1="images/services-shape-2.webp"
-                                image2="images/assets/icons/data-analyst.webp"
-                                title="Data Analyst"
-                                description=" If you happen to have any data analyst enquiries, our data analyst will be ready to answer any of your questions."
-                                classname="inner-content"
-                            />
-                            
+                            {images.map((item, index) => (
+                                <ServiceItem 
+                                    img1={item[0]}
+                                    img2={item[1]} 
+                                    title={titles[index]}
+                                    description={descriptions[index]}
+                                    descClass={descClassNames[index]}
+                                    imgClass={imgClassNames[index]}
+                                    optionalList={titles[index] === "Ruby on Rails"? offerings1 : titles[index] === "Debugging Code"? offerings2 : []}
+                                />
+                            ))}                       
                         </Row>
                     </Col>
                     <Col xs={12}>
